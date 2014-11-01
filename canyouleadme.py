cb949df22b035fc72d2ef200d55035d63a7d3b26
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-11-01 00:33:11 +0000

    created procfile

--- canyouleadme.py
+++ canyouleadme.py
@@ -24,4 +24,5 @@ def hello_monkey():
     return str(resp)
  
 if __name__ == "__main__":
-    app.run(port=3000, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
